0eef67408700df70efe34ab80afe109cdd3759fd
Kamal <theoriginalkamal@gmail.com>
2020-01-26 12:55:34 +0530

    Updates

---
+++ precache-manifest.898ee5ecea55684678a9aabbfe669430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f4cac6aac0aba6664d57de05bdd524",
+    "url": "/neet-prep/index.html"
+  },
+  {
+    "revision": "216e62ebb26da2c8524a",
+    "url": "/neet-prep/static/css/main.775440b6.chunk.css"
+  },
+  {
+    "revision": "7375f5db4eb4e351c203",
+    "url": "/neet-prep/static/js/2.8f20cd2f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/neet-prep/static/js/2.8f20cd2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "216e62ebb26da2c8524a",
+    "url": "/neet-prep/static/js/main.d115676a.chunk.js"
+  },
+  {
+    "revision": "a130a5d175458604dd83",
+    "url": "/neet-prep/static/js/runtime-main.8655a1a3.js"
+  }
+]);
